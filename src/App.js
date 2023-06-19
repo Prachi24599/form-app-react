25c27b3cae933e35793314748b9778116993cb82
Prachi24599 <prachi24599@gmail.com>
2023-06-19 13:30:13 +0530

    input field with handler added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  function changeFnameHandler(event) {
+    console.log("FirstName");
+    console.log(event.target.value);
+  }
+  function changeLnameHandler(event) {
+    console.log("LastName");
+    console.log(event.target.value);
+  }
+  return (
+    <div className="App">
+      <form>
+        <input
+          type="text"
+          placeholder="firstname"
+          onChange={changeFnameHandler}
+        />
+        <input
+          type="text"
+          placeholder="lastname"
+          onChange={changeLnameHandler}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default App;
