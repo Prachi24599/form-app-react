a8e8358f7c1f8ff26e8e3c071c214b4f906ac41c
Prachi24599 <prachi24599@gmail.com>
2023-06-20 11:51:48 +0530

    updating value of field from which event called

--- src/App.js
+++ src/App.js
@@ -2,35 +2,48 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [fname, setFName] = useState("");
-  const [lname, setLName] = useState("");
+  // const [fname, setFName] = useState("");
+  // const [lname, setLName] = useState("");
+  // console.log(fname);
+  // console.log(lname);
+  // function changeFnameHandler(event) {
+  //   // console.log("FirstName");
+  //   // console.log(event.target.value);
+  //   setFName(event.target.value);
+  // }
+  // function changeLnameHandler(event) {
+  //   // console.log("LastName");
+  //   // console.log(event.target.value);
+  //   setLName(event.target.value);
+  // }
 
-  console.log(fname);
-  console.log(lname);
-  function changeFnameHandler(event) {
-    // console.log("FirstName");
-    // console.log(event.target.value);
-    setFName(event.target.value);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+  });
+  function changeHandler(event) {
+    setFormData(function (prevFormData) {
+      return { ...prevFormData, [event.target.name]: event.target.value };
+    });
   }
-  function changeLnameHandler(event) {
-    // console.log("LastName");
-    // console.log(event.target.value);
-    setLName(event.target.value);
-  }
+
+  console.log(formData);
   return (
     <div className="App">
       <form>
         <input
           type="text"
           placeholder="firstname"
-          onChange={changeFnameHandler}
+          onChange={changeHandler}
+          name="firstName"
         />
         <br />
         <br />
         <input
           type="text"
           placeholder="lastname"
-          onChange={changeLnameHandler}
+          onChange={changeHandler}
+          name="lastName"
         />
       </form>
     </div>
